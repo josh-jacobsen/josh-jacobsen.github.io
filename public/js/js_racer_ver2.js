1bf6cf9a2cfb30453ccb40d6929ac4c3d15bc9df
josh-jacobsen <joshjacobsen5@gmail.com>
2016-05-11 21:38:44 +1200

    working torpedos

--- public/js/js_racer_ver2.js
+++ public/js/js_racer_ver2.js
@@ -12,6 +12,11 @@ window.onload = function() {
     var submarine_img = new Image();
     submarine_img.src = "../public/images/js_racer_ver2/submarine-t.png";
 
+    var torpedo_img = new Image();
+    torpedo_img.src = "../public/images/js_racer_ver2/torpedo.png";
+
+    var torpedoArray = [];
+
     var keyActions = {
         32: "fire",
         37: "left",
@@ -30,7 +35,11 @@ window.onload = function() {
         this.y = (height - 100) / 2;
         this.xSpeed = 0;
         this.ySpeed = 0;
-    }
+    };
+
+    Sub.prototype.getX = function() {
+      return this.x;
+    };
 
     Sub.prototype.draw = function() {
         ctx.drawImage(submarine_img, this.x, this.y, 100, 100);
@@ -44,8 +53,8 @@ window.onload = function() {
             }
         } else if (direction === "down") {
             this.y += 5;
-            if (this.y > (height - (height * .2))) {
-                this.y = (height - (height * .2))
+            if (this.y > (height - (height * 0.2))) {
+                this.y = (height - (height * 0.2));
             }
         } else if (direction === "left") {
             this.x += -5;
@@ -55,27 +64,48 @@ window.onload = function() {
 
         } else if (direction === "right") {
             this.x += 5;
-            if (this.x > (width - (width * .15))) {
-                this.x = width - (width * .15);
+            if (this.x > (width - (width * 0.15))) {
+                this.x = width - (width * 0.15);
             }
 
         } else if (direction === "fire") {
-            var torpedo = new Torpedo;
+            //console.log(submarine.getX);
+            console.log(submarine.x);
+            console.log(submarine.y);
+
+            var torpedo = new Torpedo();
+            torpedoArray.push(torpedo);
         }
     };
 
-    var Torpedo = function(submarine){
+    var Torpedo = function(){
         this.x = submarine.x;
         this.y = submarine.y;
         this.xSpeed = 2;
         this.ySpeed = 0;
-    }
+    };
 
-    var submarine = new Sub;
+    Torpedo.prototype.move = function() {
+      this.x += this.xSpeed;
+    };
+
+    Torpedo.prototype.draw = function() {
+      ctx.drawImage(torpedo_img, this.x + 95, this.y + 40, 20, 20);
+    };
+
+    var submarine = new Sub();
 
     setInterval(function() {
         ctx.clearRect(0, 0, width, height);
         submarine.draw();
+        console.log(torpedoArray.length);
+        for (i = 0; i < torpedoArray.length; i++) {
+          torpedoArray[i].move();
+          torpedoArray[i].draw();
+        }
+        // if (torpedoArray.length > 0) {
+        //   torpedo.draw();
+        // }
     }, 30);
 
-}
+};
---
+++ test/test.js
@@ -0,0 +1,12 @@
+alert("the js file is working");
+
+
+function sayHi(input){
+  console.log(input);
+}
+
+var multiply = function() {
+  var result = 6 * 8;
+};
+
+sayHi(Josh);
